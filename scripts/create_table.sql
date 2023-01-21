6b38ee45567a85342ade1518da40491eab6c0757
Piotrekszmel <piotr.szmelczynski@gmail.com>
2023-01-21 01:15:39 +0100

    Make create_table script more readable

--- scripts/create_table.sql
+++ scripts/create_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS gpx_tracks (  
-id INT PRIMARY KEY IDENTITY(1, 1),  
-time DATETIME NOT NULL,  
-latitude FLOAT NOT NULL,  
-longitude FLOAT NOT NULL,  
-speed FLOAT NOT NULL,  
-course FLOAT NOT NULL
+    id INT PRIMARY KEY IDENTITY(1, 1),  
+    time DATETIME NOT NULL,  
+    latitude FLOAT NOT NULL,  
+    longitude FLOAT NOT NULL,  
+    speed FLOAT NOT NULL,  
+    course FLOAT NOT NULL
 );
