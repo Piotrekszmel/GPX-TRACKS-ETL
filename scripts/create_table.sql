a58b5df7fb7621a73661f042922ce75453d2dc70
Piotrekszmel <piotr.szmelczynski@gmail.com>
2023-02-03 11:36:36 +0100

    Update create table script
    Due to data model change query was updated

--- scripts/create_table.sql
+++ scripts/create_table.sql
@@ -3,6 +3,9 @@ CREATE TABLE IF NOT EXISTS gpx_tracks (
     time DATETIME NOT NULL,  
     latitude FLOAT NOT NULL,  
     longitude FLOAT NOT NULL,  
-    speed FLOAT NOT NULL,  
-    course FLOAT NOT NULL
+    speed FLOAT,  
+    course FLOAT,
+    origin_city VARCHAR(255) NOT NULL,
+    dest_city VARCHAR(255) NOT NULL,
+    track_id  VARCHAR(255) NOT NULL 
 );
